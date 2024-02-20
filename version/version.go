27a4783ce28aec1d90388bd881cf9444b1088cc6
Martin Marsh <martin@marshtrio.com>
2024-02-20 19:08:16 +0000

    version

--- version/version.go
+++ version/version.go
@@ -1,6 +1,6 @@
 package version
 
 var (
-	BuildVersion string = "v0.2.0"
-	BuildTime    string = "Nmea-Mux lib version"
+	BuildVersion string = "v0.2.1"
+	BuildTime    string = "Nmea-Mux RPI tests"
 )
